b1053bf72b97808bbe76fdb55ba907cf8df45939
AdityaAnandCodes <adityaanandatwork276@gmail.com>
2024-12-01 12:59:30 +0530

    Added Accounts Path

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -2,6 +2,7 @@ import { Header } from "@/components/Header";
 import { useWallet } from "@aptos-labs/wallet-adapter-react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Accounts from "./pages/Accounts";
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <main className="bg-gradient-to-b from-black via-black to-gray-950 text-white min-h-screen relative">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/account" element={<Accounts />} />
         </Routes>
       </main>
     </Router>
