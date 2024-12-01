5496297520052eb12ebb07d6173f55433d0f1e9d
AdityaAnandCodes <adityaanandatwork276@gmail.com>
2024-12-01 12:42:33 +0530

    Added Home Page

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,12 +1,24 @@
 import { Header } from "@/components/Header";
+import { useWallet } from "@aptos-labs/wallet-adapter-react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 
 function App() {
-  return (
-    <>
-      
-      
-    </>
+  const { connected } = useWallet();
+
+  return connected ? (
+    <Router>
+      <main className="bg-gradient-to-b from-black via-black to-gray-950 text-white min-h-screen relative">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </main>
+    </Router>
+  ) : (
+    <main className="bg-neutral-900 text-white min-h-screen overflow-hidden">
+      <Header />
+    </main>
   );
 }
 
