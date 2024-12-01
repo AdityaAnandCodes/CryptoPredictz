078aff7a581a486074c5117b5804414f38e4972d
AdityaAnandCodes <adityaanandatwork276@gmail.com>
2024-12-01 18:16:51 +0530

    added Hero Section

--- frontend/components/Hero2.tsx
+++ frontend/components/Hero2.tsx
@@ -2,20 +2,21 @@ import React from "react";
 
 const Hero: React.FC = () => {
   return (
-    <div className="bg-gray-900">
+    <div className="min-h-fit p-5">
       <div className="flex w-full">
         {/* Block 1 */}
-        <div className="flex-1 h-40 bg-red-500 rounded-lg shadow-lg flex items-center justify-center text-white text-lg font-bold mx-2">
+        <div className="flex-1 h-40 hero-1 rounded-lg shadow-lg flex items-center justify-center text-white text-lg font-bold mx-2">
+        
           Cashback Offers
         </div>
 
         {/* Block 2 */}
-        <div className="flex-1 h-40 bg-blue-500 rounded-lg shadow-lg flex items-center justify-center text-white text-lg font-bold mx-2">
+        <div className="flex-1 h-40 hero-2 rounded-lg shadow-lg flex items-center justify-center text-white text-lg font-bold mx-2">
           Rakeback Deals
         </div>
 
         {/* Block 3 */}
-        <div className="flex-1 h-40 bg-green-500 rounded-lg shadow-lg flex items-center justify-center text-white text-lg font-bold mx-2">
+        <div className="flex-1 h-40 hero-3 rounded-lg shadow-lg flex items-center justify-center text-white text-lg font-bold mx-2">
           Payment Methods
         </div>
       </div>
