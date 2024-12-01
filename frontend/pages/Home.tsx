0da8b593c80f4d59adf5dd25422c9985490c1ccc
raksha-bv <rakshabv04@gmail.com>
2024-12-01 13:47:27 +0530

    updating home

--- frontend/pages/Home.tsx
+++ frontend/pages/Home.tsx
@@ -2,8 +2,6 @@ import { Header } from "@/components/Header";
 import Navbar from "@/components/Navbar";
 import Faq from "@/components/Faq";
 import Features from "@/components/Features";
-
-import UpcomingMatches from "@/components/UpcomingMatches";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 
@@ -14,7 +12,6 @@ const Home = () => {
       <Hero />
       <Navbar />
       <Features />
-      <UpcomingMatches />
       <Faq />
       <Footer />
     </main>
