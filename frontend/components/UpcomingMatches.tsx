470564452b26a369f1ea6057032a2ab1b23e404b
raksha-bv <rakshabv04@gmail.com>
2024-12-01 12:53:20 +0530

    upcoming matches list

---
+++ frontend/components/UpcomingMatches.tsx
@@ -0,0 +1,28 @@
+import BettingCard from "./BettingCard";
+import { BettingCardDummy } from "@/constants";
+
+export default function UpcomingMatches() {
+  return (
+    <div className="text-white p-4 pt-2">
+      <h2 className="text-xl font-semibold  text-white mb-4">🔥Upcoming Matches</h2>
+      <div className="flex space-x-4 overflow-x-auto scrollbar-hide">
+        {" "}
+        {BettingCardDummy.map((match, index) => (
+          <div key={index} className="flex-shrink-0 w-[300px] mb-4">
+            {" "}
+            {/* Adjusted width for bigger cards */}
+            <BettingCard
+              title={match.game}
+              time={match.when}
+              team1={match.teamA}
+              team1Image={match.teamAImage}
+              team2={match.teamB}
+              team2Image={match.teamBImage}
+              odds={match.odds}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
--- frontend/pages/Home.tsx
+++ frontend/pages/Home.tsx
@@ -1,27 +1,12 @@
 import { Header } from "@/components/Header";
 
-import Hero from "@/components/Hero";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
-import Features from "@/components/Features";
 import UpcomingMatches from "@/components/UpcomingMatches";
-import Faq from "@/components/Faq";
-
-import BlackjackGame from "../../games/blackjack";
-import SlotMachine from "../../games/slotMachine";
 
 const Home = () => {
   return (
     <main className="bg-gradient-to-b from-black via-black to-gray-950 text-white min-h-screen relative">
       <Header />
-      <Hero />
-      <Navbar />
-      <BlackjackGame />
-      <SlotMachine />
       <UpcomingMatches />
-      <Features />
-      <Faq />
-      <Footer />
     </main>
   );
 };
